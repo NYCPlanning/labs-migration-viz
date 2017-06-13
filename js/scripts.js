acb648d28fd9821f888387919bee06a906601220
Chris Whong <chris@whong.org>
2017-06-13 13:28:44 -0400

    added composable chart

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,47 @@
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 60},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+function renderChart(el, data) {
+  var yMin = -300000,
+      yMax = 300000;
 
-// set the ranges
-var x = d3.scaleBand()
-          .range([0, width])
-          .padding(0.1);
-var y = d3.scaleLinear()
-          .range([height, 0]);
+  // set the dimensions and margins of the graph
+  var margin = {top: 20, right: 20, bottom: 30, left: 60},
+      width = 300 - margin.left - margin.right,
+      height = 200 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-// append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+  // set the ranges
+  var x = d3.scaleBand()
+            .range([0, width])
+            .padding(0.1);
+  var y = d3.scaleLinear()
+            .range([height, 0]);
 
-// get the data
-d3.json("data/age.json", function(error, data) {
-  if (error) throw error;
+  // append the svg object to the body of the page
+  // append a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3.select(el).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+
 
   // // // format the data
   // data.age.forEach(function(d) {
   //   d.out = +d.out;
   // });
 
-  var age = data.age
 
-  console.log(age);
+
+  console.log(data);
 
   // Scale the range of the data in the domains
-  x.domain(age.map(function(d) { return d.label; }));
-  y.domain([
-    -d3.max(age, function(d) { return d.out; }),
-    d3.max(age, function(d) { return d.in; })
-  ]);
+  x.domain(data.map(function(d) { return d.label; }));
+  y.domain([yMin, yMax]);
 
   // append the inflow rectangles
   svg.selectAll(".in")
-      .data(age)
+      .data(data)
     .enter().append("rect")
       .attr("class", "bar in")
       .attr("x", function(d) { return x(d.label); })
@@ -52,7 +51,7 @@ d3.json("data/age.json", function(error, data) {
 
   // append the outflow rectangles
   svg.selectAll(".out")
-      .data(age)
+      .data(data)
     .enter().append("rect")
       .attr("class", "bar out")
       .attr("x", function(d) { return x(d.label); })
@@ -60,6 +59,16 @@ d3.json("data/age.json", function(error, data) {
       .attr("y", function(d) { return y(0); })
       .attr("height", function(d) { return y(0) - y(d.out); });
 
+
+  // add non-labeled x-axis line at 0
+
+  svg.append("line")
+    .attr("class", "center-axis")
+    .attr("x1", 0 )
+    .attr("y1", y(0))
+    .attr("x2", width)
+    .attr("y2", y(0))
+    .attr("width", width);
 
   // add the x Axis
   svg.append("g")
@@ -70,4 +79,16 @@ d3.json("data/age.json", function(error, data) {
   svg.append("g")
       .call(d3.axisLeft(y));
 
+
+}
+
+// get the data
+d3.json("data/age.json", function(error, data) {
+  if (error) throw error;
+  renderChart("body", data['1935_1940'])
+});
+
+d3.json("data/age.json", function(error, data) {
+  if (error) throw error;
+  renderChart("body", data['1975_1980'])
 });
