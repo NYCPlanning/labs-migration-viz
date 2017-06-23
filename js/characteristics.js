834fbed507c06937da77b535555c3cbf1466c652
kschmidtDCP <kschmidt@planning.nyc.gov>
2017-06-23 17:00:33 -0400

    Updated dummy text with first draft of characteristics observations

--- js/characteristics.js
+++ js/characteristics.js
@@ -2,55 +2,55 @@ var characteristics = { // eslint-disable-line
   age: {
     displayName: 'Age',
     colors: ['#deebf7', '#9ecae1', '#3182bd'],
-    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim nisl vitae sem blandit vestibulum. Nullam nunc felis, hendrerit vel pretium in, egestas non lacus. Aenean at turpis ultricies, congue arcu vel, luctus arcu.',
+    about: 'Of all characteristics, age is one of the greatest predictors of migration. NYC consistently attracts large numbers of people in their 20s, and generally sees net migration losses of people in all other age groups. This is tied to the lifecycle of migration, whereby young single people move to the city, and leave after family formation.',
     descriptor: 'people aged'
   },
   sex: {
     displayName: 'Sex',
     colors: ['#deebf7', '#9ecae1', '#3182bd'],
-    about: 'Cras vel lobortis velit. In non nunc nec lorem ultrices placerat egestas sed sem. Curabitur blandit eleifend pretium. Sed varius luctus tincidunt. Ut fringilla tristique sapien, at fringilla metus semper imperdiet. Nullam imperdiet maximus sem et faucibus. Mauris semper urna nunc.',
+    about: 'Sex is weakly correlated with migration. Historically, males and females are generally equally likely to move in or out of the city. However, between 2010 and 2014 NYC experienced a net gain of 44,000 more women than men due to migration.',
     descriptor: 'people who reported as'
   },
   race_and_hispanic_origin: {
     displayName: 'Race and Hispanic Origin',
     colors: ['#deebf7', '#9ecae1', '#3182bd'],
-    about: 'Cras vel lobortis velit. In non nunc nec lorem ultrices placerat egestas sed sem. Curabitur blandit eleifend pretium. Sed varius luctus tincidunt. Ut fringilla tristique sapien, at fringilla metus semper imperdiet. Nullam imperdiet maximus sem et faucibus. Mauris semper urna nunc.',
+    about: 'Migration by race and Hispanic origin groups is closely tied to national socioeconomic trends. During the city&apos;s fiscal crisis in the late 70s and through 1990, out-migration was high for most groups, particularly whites. Asian immigration played a significant role in stabilizing population losses during this time and has been the only group to see net positive migration for all periods since 1975.',
     descriptor: 'people who reported their race as'
   },
   nativity: {
     displayName: 'Nativity',
     colors: ['#deebf7', '#9ecae1', '#3182bd'],
-    about: 'Cras vel lobortis velit. In non nunc nec lorem ultrices placerat egestas sed sem. Curabitur blandit eleifend pretium. Sed varius luctus tincidunt. Ut fringilla tristique sapien, at fringilla metus semper imperdiet. Nullam imperdiet maximus sem et faucibus. Mauris semper urna nunc.',
+    about: 'Nativity of migrants reflects the significance of immigration to NYC. Through all periods where data is available, NYC saw large net gains of the foreign born population, while experiencing a net loss of native born persons. This illustrates NYC&apos;s historic role in providing a landing place for newcomers to the US and in turn sending native born people to the rest of the country.',
     descriptor: 'people who were'
   },
   marital_status: {
     displayName: 'Marital Status',
     colors: ['#deebf7', '#9ecae1', '#3182bd'],
-    about: 'Cras vel lobortis velit. In non nunc nec lorem ultrices placerat egestas sed sem. Curabitur blandit eleifend pretium. Sed varius luctus tincidunt. Ut fringilla tristique sapien, at fringilla metus semper imperdiet. Nullam imperdiet maximus sem et faucibus. Mauris semper urna nunc.',
+    about: 'Lifecycle migration is particularly evident through marital status data, which is highly correlated with age. Through all periods measured, NYC attracts large amounts of people who have never been married, and in turn sees many married people leave. 2010-14 is the first period in our data showing net migration close to zero of married people however, indicating that families may be seeing the city differently than in the past. People who are married but with an absent spouse are often immigrants bring family over in stages, and therefore show net positive migration.',
     descriptor: 'people who reported as'
   },
   educational_attainment: {
     displayName: 'Educational Attainment',
     colors: ['#deebf7', '#9ecae1', '#3182bd'],
-    about: 'Cras vel lobortis velit. In non nunc nec lorem ultrices placerat egestas sed sem. Curabitur blandit eleifend pretium. Sed varius luctus tincidunt. Ut fringilla tristique sapien, at fringilla metus semper imperdiet. Nullam imperdiet maximus sem et faucibus. Mauris semper urna nunc.',
+    about: 'Since 1975, migration flows to and from NYC have favored the undereducated as well as the highly educated, whereas high school graduates and those with some college see the greatest losses. This continues to be true today, and is a reflection of the city&apos;s bifurcated economy, dominated by large numbers of skilled professionals as well as low skill workers who are often immigrants.',
     descriptor: 'people whose educational attainment was'
   },
   earnings: {
     displayName: 'Earnings',
     colors: ['#deebf7', '#9ecae1', '#3182bd'],
-    about: 'Cras vel lobortis velit. In non nunc nec lorem ultrices placerat egestas sed sem. Curabitur blandit eleifend pretium. Sed varius luctus tincidunt. Ut fringilla tristique sapien, at fringilla metus semper imperdiet. Nullam imperdiet maximus sem et faucibus. Mauris semper urna nunc.',
+    about: 'A description for earnings is coming soon.',
     descriptor: 'people whose earnings were'
   },
   occupation: {
     displayName: 'Occupation',
     colors: ['#deebf7', '#9ecae1', '#3182bd'],
-    about: 'Cras vel lobortis velit. In non nunc nec lorem ultrices placerat egestas sed sem. Curabitur blandit eleifend pretium. Sed varius luctus tincidunt. Ut fringilla tristique sapien, at fringilla metus semper imperdiet. Nullam imperdiet maximus sem et faucibus. Mauris semper urna nunc.',
+    about: 'A description for occupation is coming soon.',
     descriptor: 'people whose occupation was'
   },
   industry: {
     displayName: 'Industry',
     colors: ['#deebf7', '#9ecae1', '#3182bd'],
-    about: 'Cras vel lobortis velit. In non nunc nec lorem ultrices placerat egestas sed sem. Curabitur blandit eleifend pretium. Sed varius luctus tincidunt. Ut fringilla tristique sapien, at fringilla metus semper imperdiet. Nullam imperdiet maximus sem et faucibus. Mauris semper urna nunc.',
+    about: 'A description for industry is coming soon.',
     descriptor: 'people whose industry was'
   }
 };
